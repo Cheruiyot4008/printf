79009b14b7f586cda0aeb67c67d480b45b79325d
cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-10-16 09:48:35 -0700

    first task

---
+++ first.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...)
+{
+	int printed_chars = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	if (format == NULL)
+		return (0);
+
+	while (*format)
+	{
+		if (*format == '%' && (*(format ++) == 'c' || *(format ++) == 's' || *(format ++) == '%'))
+		{
+			format+1;
+			switch (*format)
+			{
+				case 'c':
+					printed_chars += write(1, &va_arg(args, int), 1);
+					break;
+				case 's':
+				{
+					char *str = va_arg(args, char *);
+					str = (str == NULL) ? "(null)" : str;
+					while (*str)
+						printed_chars += write(1, str++, 1);
+					break;
+				}
+				case '%':
+					printed_chars += write(1, "%", 1);
+					break;
+			}
+		}
+		else
+		{
+			printed_chars += write(1, format, 1);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (printed_chars);
+}
