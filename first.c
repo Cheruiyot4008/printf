78ffb2a7068f825bf7e7b2225d7e1fb349e401f3
cheruiyot4008 <cheruiyotbrian454@gmail.com>
2023-10-16 11:42:32 -0700

    files

--- first.c
+++ first.c
@@ -2,46 +2,54 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * _printf - Custom printf function with limited conversion specifiers.
+ * @format: Format string containing conversion specifiers.
+ *
+ * Return: Number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
-	int printed_chars = 0;
-	va_list args;
+    int printed_chars = 0;
+    va_list args;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	if (format == NULL)
-		return (0);
+    if (format == NULL)
+        return (0);
 
-	while (*format)
-	{
-		if (*format == '%' && (*(format ++) == 'c' || *(format ++) == 's' || *(format ++) == '%'))
-		{
-			format+1;
-			switch (*format)
-			{
-				case 'c':
-					printed_chars += write(1, &va_arg(args, int), 1);
-					break;
-				case 's':
-				{
-					char *str = va_arg(args, char *);
-					str = (str == NULL) ? "(null)" : str;
-					while (*str)
-						printed_chars += write(1, str++, 1);
-					break;
-				}
-				case '%':
-					printed_chars += write(1, "%", 1);
-					break;
-			}
-		}
-		else
-		{
-			printed_chars += write(1, format, 1);
-		}
-		format++;
-	}
+    while (*format)
+    {
+        if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%'))
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    printed_chars += write(1, &va_arg(args, int), 1);
+                    break;
+                case 's':
+                {
+                    char *str = va_arg(args, char *);
+                    str = (str == NULL) ? "(null)" : str;
+                    while (*str)
+                    {
+                        printed_chars += write(1, str++, 1);
+                    }
+                    break;
+                }
+                case '%':
+                    printed_chars += write(1, "%", 1);
+                    break;
+            }
+        }
+        else
+        {
+            printed_chars += write(1, format, 1);
+        }
+        format++;
+    }
 
-	va_end(args);
-	return (printed_chars);
+    va_end(args);
+    return (printed_chars);
 }
